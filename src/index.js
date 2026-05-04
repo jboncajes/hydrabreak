74ca847e7a071a697705cabf4c65b558dc79e6eb
jboncajes <johnrusselboncajes@gmail.com>
2026-05-04 15:33:29 +0800

    Set homepage for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -12,12 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
 
-// Register service worker for PWA / offline support
 serviceWorker.register();
